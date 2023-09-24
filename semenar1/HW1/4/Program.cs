60a95a701a93dc6f9387a4439baab742723e9ecf
Olga <lelya860212@gmail.com>
2023-09-24 16:04:10 +0300

    четвертое  задание

---
+++ semenar1/HW1/4/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число");
+string Number=Console.ReadLine()!;
+int NumberInt=Convert.ToInt32(Number);
+int NumberBegin=1;
+ if (NumberInt<NumberBegin)
+     {
+     Console.Write("Число должно быть больше ");
+     Console.Write(NumberBegin);
+     }
+     Console.WriteLine("Четные числа: ");
+while (NumberBegin<=NumberInt)
+{
+   int Remainder=NumberBegin%2;
+    if (Remainder==0) 
+        {
+            Console.WriteLine(NumberBegin);
+        }
+   NumberBegin=NumberBegin+1;
+}
