c0f84293dd1db5456918adffcfd13e604278ec12
Olga <lelya860212@gmail.com>
2023-09-24 15:25:48 +0300

    Третье задание

---
+++ semenar1/HW1/3/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.WriteLine("Введите число");
+string Number1 = Console.ReadLine()!;
+int NumberInt1=Convert.ToInt32(Number1);
+int Remainder=NumberInt1%2;
+ if (Remainder==0)
+     {
+     Console.WriteLine("Число является четным.");
+     }
+ else
+   {
+     Console.WriteLine("Число является нечетным.");
+     }
