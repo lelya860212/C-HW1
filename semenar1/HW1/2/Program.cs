9850ae2b1486120d34477cdbdb3f852debb33d1e
Olga <lelya860212@gmail.com>
2023-09-24 15:04:19 +0300

    Второе задание

---
+++ semenar1/HW1/2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите первое число");
+string Number1 = Console.ReadLine()!;
+Console.WriteLine("Введите второе число");
+string Number2 = Console.ReadLine()!;
+Console.WriteLine("Введите третье число");
+string Number3 = Console.ReadLine()!;
+int NumberInt1=Convert.ToInt32(Number1);
+int NumberInt2=Convert.ToInt32(Number2);
+int NumberInt3=Convert.ToInt32(Number3);
+int MaxNumber=NumberInt1;
+if (NumberInt2 > MaxNumber)
+    {
+    MaxNumber=NumberInt2;
+    }
+if (NumberInt3 > MaxNumber)
+    {
+    MaxNumber=NumberInt3;
+    }
+Console.WriteLine("Максимальное число равно");
+Console.WriteLine(MaxNumber);
